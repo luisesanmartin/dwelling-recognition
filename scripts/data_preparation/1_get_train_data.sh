87a339dad7408653f02a1141b7a1a8d9e963ac99
Luis Eduardo San Martin <lsanmartin@uchicago.edu>
2020-03-12 16:15:32 -0500

    divided get data into two train/test data files

---
+++ scripts/data_preparation/1_get_train_data.sh
@@ -0,0 +1,2 @@
+wget -P ./../../data/raw/ "https://s3.amazonaws.com/drivendata/data/60/public/train_metadata.csv"
+wget -P ./../../data/raw/ "https://drivendata-public-assets.s3.amazonaws.com/train_tier_1.tgz"
