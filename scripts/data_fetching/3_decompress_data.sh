4ca4b1ff78d090d0733c5ee9b7c6b92e1b67a099
Luise San Martin Alegria <lsanmartin@cs.uchicago.edu>
2020-06-26 17:57:13 -0500

    added options for sbatch to decompress script

--- scripts/data_fetching/3_decompress_data.sh
+++ scripts/data_fetching/3_decompress_data.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+#
+#SBATCH --workdir=//scratch/lsanmartin/dwelling-recognition/scripts/data_fetching
+#SBATCH --job-name=decompressing_tif_files
+#SBATCH --mail-user=lsanmartin@cs.uchicago.edu
+#SBATCH --mail-type=ALL
+#SBATCH --partition=general
+#SBATCH --mem-per-cpu=15000
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 tar -xvf ./../../data/raw/train_tier_1.tgz -C ./../../data/raw train_tier_1/kam
