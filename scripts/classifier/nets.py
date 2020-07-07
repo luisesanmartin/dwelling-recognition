accf9e7a185c27e55b1ace4993bd791cc436c21e
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2020-07-07 12:35:30 -0500

    zero stride

--- scripts/classifier/nets.py
+++ scripts/classifier/nets.py
@@ -13,16 +13,16 @@ class net(nn.Module):
 		else:
 			device = torch.device('cpu')
 
-		self.conv1 = nn.Conv2d(4, 64, 5)
-		self.conv2 = nn.Conv2d(64, 256, 1)
-		self.conv3 = nn.Conv2d(256, 128, 1)
-		self.conv4 = nn.Conv2d(128,  n_classes, 1)
+		self.conv1 = nn.Conv2d(4, 64, 5, stride=0)
+		self.conv2 = nn.Conv2d(64, 256, 1, stride=0)
+		self.conv3 = nn.Conv2d(256, 128, 1, stride=0)
+		self.conv4 = nn.Conv2d(128,  n_classes, 1, stride=0)
 		self.logsoftmax = nn.LogSoftmax(dim=1)
 
 	def forward(self, x):
 		x = F.relu(self.conv1(x))
 		x = F.relu(self.conv2(x))
 		x = F.relu(self.conv3(x))
-		x = self.logsoftmax(x)
+		#x = self.logsoftmax(x)
 
 		return x
