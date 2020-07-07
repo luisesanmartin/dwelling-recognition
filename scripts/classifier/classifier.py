c7b12bfb4ac396414f86900c319963b4cc9c19d3
Luise San Martin Alegria <lsanmartin@cs.uchicago.edu>
2020-07-07 16:43:48 -0500

    Added df output for loss function results

--- scripts/classifier/classifier.py
+++ scripts/classifier/classifier.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+import pandas as pd
 from nets import net
 from loader import dwellingsDataset
 
@@ -15,7 +16,7 @@ def loss_nll(predicts, targets):
 
     return output
 
-def train(dataset, model, optimizer, epoch=None, model_name='../../models/kampala_classifier.pkl'):
+def train(dataset, model, optimizer, epoch=None, model_name='../../models/kampala_classifier.pkl', loss_df='../../models/kampala_classifier_losses.csv'):
     '''
     '''
 
@@ -23,6 +24,8 @@ def train(dataset, model, optimizer, epoch=None, model_name='../../models/kampal
         torch.cuda.empty_cache()
     model = model.to(device=device)
     model.train()
+
+    df = pd.DataFrame(columns=['epoch', 'iteration', 'loss'])
 
     for idx, (x, y) in enumerate(dataset):
         x = x.float()
@@ -34,19 +37,22 @@ def train(dataset, model, optimizer, epoch=None, model_name='../../models/kampal
         loss = loss_nll(output, y)
         loss.backward()
         optimizer.step()
+        size = len(df)
+        df.loc[size] = [epoch, idx, loss.item()]
 
         if epoch and idx % 20 == 0:
-            print('Epoch # %d, Loss: %.4f' (epoch+1, loss.item()))
+            print("Epoch %d, Loss: %.4f" % (epoch+1, loss.item()))
 
-    # Saving the model
+    # Saving the model and  loss df
     torch.save(model, model_name)
+    df.to_csv(loss_df, index=False)
 
     # Evaluating accuracy
     model.eval()
     output = model(x)
-    predictions = torch.argmax(output.data, dim=1).numpy()
-    num_correct = (predictions == y.numpy()).sum()
-    num_samples = y.size(0)
+    predictions = torch.argmax(output.data, dim=1).cpu().numpy()
+    num_correct = (predictions == y.cpu().numpy()).sum()
+    num_samples = y.size(0) * y.size(1) * y.size(2)
     print('\nEpoch #', epoch)
     print('Accuracy:', round(num_correct/num_samples*100, 2), '%')
     print('\n')
@@ -57,7 +63,7 @@ def main():
     optimizer = optim.Adam(classifier.parameters(), lr=1e-4)
     dataset = dwellingsDataset()
     data = DataLoader(dataset, batch_size=100, shuffle=True, num_workers=4)
-    epochs = 10
+    epochs = 50
 
     for epoch in range(epochs):
         train(data, classifier, optimizer, epoch)
