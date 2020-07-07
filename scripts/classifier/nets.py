09cc74f01bd5b5384145cd805b8c3db41fdace9c
Luise San Martin Alegria <lsanmartin@cs.uchicago.edu>
2020-07-07 13:09:23 -0500

    Added fourth layer in convnet

--- scripts/classifier/nets.py
+++ scripts/classifier/nets.py
@@ -13,16 +13,18 @@ class net(nn.Module):
 		else:
 			device = torch.device('cpu')
 
-		self.conv1 = nn.Conv2d(4, 64, 5, stride=0)
-		self.conv2 = nn.Conv2d(64, 256, 1, stride=0)
-		self.conv3 = nn.Conv2d(256, 128, 1, stride=0)
-		self.conv4 = nn.Conv2d(128,  n_classes, 1, stride=0)
+		self.n_classes = n_classes
+		self.conv1 = nn.Conv2d(4, 64, 1)
+		self.conv2 = nn.Conv2d(64, 256, 1)
+		self.conv3 = nn.Conv2d(256, 128, 1)
+		self.conv4 = nn.Conv2d(128,  self.n_classes, 1)
 		self.logsoftmax = nn.LogSoftmax(dim=1)
 
 	def forward(self, x):
 		x = F.relu(self.conv1(x))
 		x = F.relu(self.conv2(x))
 		x = F.relu(self.conv3(x))
-		#x = self.logsoftmax(x)
+		x = F.relu(self.conv4(x))
+		x = self.logsoftmax(x)
 
 		return x
