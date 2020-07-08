9d18ade96ffdf8356d761cb370482c927f795482
Luis Eduardo San Martin <luise.sanmartin@gmail.com>
2020-07-08 10:28:14 -0500

    Modified net

--- scripts/classifier/nets.py
+++ scripts/classifier/nets.py
@@ -3,30 +3,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class net(nn.Module):
-	'''
-	'''
+    '''
+    '''
 
-	def __init__(self, n_classes=2):
-		super(net, self).__init__()
-		if torch.cuda.is_available():
-			device = torch.device('cuda')
-		else:
-			device = torch.device('cpu')
+    def __init__(self, n_classes=2):
+        super(net, self).__init__()
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        else:
+            device = torch.device('cpu')
 
-		self.n_classes = n_classes
-		self.batchnorm1 = nn.BatchNorm2d(4, affine=False)
-		self.conv1 = nn.Conv2d(4, 64, 1)
-		self.conv2 = nn.Conv2d(64, 256, 1)
-		self.conv3 = nn.Conv2d(256, 128, 1)
-		self.conv4 = nn.Conv2d(128,  self.n_classes, 1)
-		self.logsoftmax = nn.LogSoftmax(dim=1)
+        self.n_classes = n_classes
+        self.conv1 = nn.Conv2d(4, 64, 1, kernel_size=3, padding=1, padding_mode='reflect')
+        self.batchnorm1 = nn.BatchNorm2d(64, affine=False)
+        self.conv2 = nn.Conv2d(64, 128, 1)
+        self.batchnorm2 = nn.BatchNorm2d(128, affine=False)
+        self.conv3 = nn.Conv2d(128, 64, 1)
+        self.batchnorm3 = nn.BatchNorm2d(64, affine= False)
+        self.conv4 = nn.Conv2d(64,  self.n_classes, 1)
+        self.batchnorm4 = nn.BatchNorm2d(self.n_classes, affine=False)
+        self.logsoftmax = nn.LogSoftmax(dim=1)
 
-	def forward(self, x):
-		x = self.batchnorm1(x)
-		x = F.max_pool2d(F.relu(self.conv1(x)), 4)
-		#x = F.relu(self.conv2(x))
-		#x = F.relu(self.conv3(x))
-		#x = F.relu(self.conv4(x))
-		#x = self.logsoftmax(x)
+    def forward(self, x):
+        x = self.batchnorm1(self.conv1(x))
+        x = F.max_pool2d(F.relu(x), 2)
+        x = self.batchnorm2(self.conv2(x))
+        x = F.max_pool2d(F.relu(x), 2)
+        x = self.batchnorm3(self.conv3(x))
+        x = F.max_pool2d(F.relu(x), 2)
+        x = self.batchnorm4(self.conv4(x))
+        x = F.max_pool2d(F.relu(x), 2)
 
-		return x
+        return x
